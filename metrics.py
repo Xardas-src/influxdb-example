1b12d92559fb5a21c5e078b8d68e1b04cf67034f
r2d2-lex <r2d2-lex@yandex.ru>
2020-10-30 10:53:37 +0000

    influxdb cpuload

---
+++ metrics.py
@@ -0,0 +1,42 @@
+from influxdb import InfluxDBClient
+from time import sleep
+import psutil
+
+client = InfluxDBClient(
+    database='testdb1',
+    host='devserver',
+    username='super',
+    password='user',
+)
+
+
+def get_cpu_load():
+    cores = psutil.cpu_percent(percpu=True)
+    data = {}
+    for core, load in enumerate(cores):
+        data['cpu{}'.format(core)] = load
+    return data
+
+
+def log_cpu_load():
+    cpus = get_cpu_load()
+    json_data = []
+    for cpu, load in cpus.items():
+        json_data.append({
+            'measurement': 'cpu_usage',
+            'tags': {
+                'server': 'localhost',
+                'cpu': cpu,
+            },
+            'fields': {
+                'value': load,
+            }
+        })
+    return client.write_points(json_data)
+
+
+if __name__ == '__main__':
+    while True:
+        log_cpu_load()
+        sleep(1)
+
